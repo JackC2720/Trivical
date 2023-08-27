d84c956d344c4a7547d1114d7f5af54a9389b0af
Jack Chamberlain <JChamberlain@carbonsix.digital>
2023-08-28 00:02:09 +0100

    View component scafolding

---
+++ Trivical.Core/Components/QuestionsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trivical.Core.Components
+{
+	public class QuestionsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<string> products = new List<string>() {
+                "Product 1", "Product 2", "Product 3", "Product 4", "Product 5"
+            };
+
+            return View(products);
+        }
+    }
+}
