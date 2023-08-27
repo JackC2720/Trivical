3eed46ce596b77603d9ebe040d176d2e9a48d512
Jack Chamberlain <JChamberlain@carbonsix.digital>
2023-08-28 00:47:35 +0100

    Question object and api response

--- Trivical.Core/Components/QuestionsViewComponent.cs
+++ Trivical.Core/Components/QuestionsViewComponent.cs
@@ -1,16 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Trivical.Core.Models.ViewModels;
+
 
 namespace Trivical.Core.Components
 {
 	public class QuestionsViewComponent : ViewComponent
-    {
-        public async Task<IViewComponentResult> InvokeAsync()
-        {
-            List<string> products = new List<string>() {
-                "Product 1", "Product 2", "Product 3", "Product 4", "Product 5"
-            };
+	{
+		private readonly HttpClient _httpClient;
 
-            return View(products);
-        }
-    }
+		public QuestionsViewComponent(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			// Use HttpClient to fetch data from the API
+			HttpResponseMessage response = await _httpClient.GetAsync("https://opentdb.com/api.php?amount=10&type=multiple");
+
+			if (response.IsSuccessStatusCode)
+			{
+				var content = await response.Content.ReadAsStringAsync();
+                // Deserialize JSON response
+                var triviaQuestions = JsonConvert.DeserializeObject<Root>(content);
+
+                ViewReturn viewReturn = new ViewReturn();
+                viewReturn.questions = new List<Question>();
+
+                foreach (var questionResult in triviaQuestions.results)
+                {
+                    Question question = new Question
+                    {
+                        question = questionResult.question,
+                        answers = new List<Answers>()
+                    };
+
+                    // Add correct answer
+                    question.answers.Add(new Answers
+                    {
+                        answer = questionResult.correct_answer,
+                        correct = true
+                    });
+
+                    // Add incorrect answers
+                    foreach (var incorrectAnswer in questionResult.incorrect_answers)
+                    {
+                        question.answers.Add(new Answers
+                        {
+                            answer = incorrectAnswer,
+                            correct = false
+                        });
+                    }
+
+                    viewReturn.questions.Add(question);
+                }
+
+                return View(viewReturn);
+			}
+			else
+			{
+				// Handle error case
+				return View(new Root());
+			}
+		}
+	}
 }
